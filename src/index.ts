d82d536ce830a0633350a87c77400c71b92fc376
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2025-08-04 15:56:55 -0400

    patch: permisos de cors desde prueba-tecnica.pages.dev y localhost

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Hono } from 'hono'
 import {getCharacter} from "./lib/rick-and-morty-api";
+import { cors } from 'hono/cors'
+
 
 const app = new Hono()
+app.use('/*', cors({
+  origin: ['https://prueba-tecnica.pages.dev', 'http://localhost:5173'],
+  allowMethods: ['POST', 'GET', 'OPTIONS'],
+}))
 
 app.get('/:id', async (c) => {
   const id = c.req.param('id') // Obtiene el id de la solicitud.
